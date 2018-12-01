4a8a611b01e516bb1e4aaf736c596e449c02ad3d
nullthefirst <jrandangana@gmail.com>
2018-12-01 15:14:59 +0100

    edited block

--- src/components/views/block.js
+++ src/components/views/block.js
@@ -16,14 +16,6 @@ export default class Block extends Component {
         <Four />
         <Five />
 
-        <hr />
-
-        <a href="https://afrolumia.club/" alt="
-        https://afrolumia.club/"><h4>AfroLumia <i class="em em-link"></i></h4></a>
-        <p>AfroLumia is a creative arts platform that actively supports innovative lifestyles. Think digital bohemians, think technologically innovative young people. Youth is a thing of the mind.</p>
-          
-        <hr />
-
         <a href="https://poetrique.github.io/" alt="https://poetrique.github.io/"><h4>Poetrique Online <i class="em em-link"></i></h4></a>
         <p>Poetrique is a creative arts brand, that focuses on creating unique user experiences and interactions for the web. We are the bridge between technology and art. We work with world-class technology to produce amazing results.</p>
 
--- src/components/views/git/six.js
+++ src/components/views/git/six.js
@@ -1,10 +1,12 @@
 import React,  { Component } from 'react';
 
-export default class One extends Component {
+export default class Six extends Component {
   render() {
     return(
       <div className="card">
-        #
+        <a href="https://afrolumia.club/" alt="
+        https://afrolumia.club/"><h4>AfroLumia <i class="em em-link"></i></h4></a>
+        <p>AfroLumia is a creative arts platform that actively supports innovative lifestyles. Think digital bohemians, think technologically innovative young people. Youth is a thing of the mind.</p>
       </div>
     )
   }
