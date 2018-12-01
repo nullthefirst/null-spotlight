9b763df2dcf274ef3865db03296415026dcb2a83
nullthefirst <jrandangana@gmail.com>
2018-12-01 19:28:01 +0100

    added social.js

---
+++ src/components/views/social.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+export default class Social extends Component {
+  render() {
+    return(
+      <div>
+        <a href="https://linkedin.com/in/usheninte" alt="LinkedIn" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-linkedin fa-2x social"></i>
+        </a>
+        <a href="https:/github.com/Usheninte" alt="Github" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-github fa-2x social"></i>
+        </a>
+        <a href="https://twitter.com/Usheninte" alt="Twitter" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-twitter fa-2x social"></i>
+        </a>
+      </div>
+    );
+  }
+}
