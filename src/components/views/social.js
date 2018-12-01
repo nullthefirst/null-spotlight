ee9a2b5f2e90fc873567643a0cd576fcacc6ad0e
nullthefirst <jrandangana@gmail.com>
2018-12-01 22:41:22 +0100

    added social.js

--- src/components/views/social.js
+++ src/components/views/social.js
@@ -6,13 +6,13 @@ export default class Social extends Component {
   render() {
     return(
       <div>
-        <a href="#" alt="LinkedIn" target="_blank" rel="noopener noreferrer">
+        <a href="https://poetrique.gitub.io" alt="LinkedIn" target="_blank" rel="noopener noreferrer">
           <i class="fab fa-linkedin fa-2x social"></i>
         </a>
-        <a href="#" alt="Github" target="_blank" rel="noopener noreferrer">
+        <a href="https://poetrique.gitub.io" alt="Github" target="_blank" rel="noopener noreferrer">
           <i class="fab fa-github fa-2x social"></i>
         </a>
-        <a href="#" alt="Twitter" target="_blank" rel="noopener noreferrer">
+        <a href="https://poetrique.gitub.io" alt="Twitter" target="_blank" rel="noopener noreferrer">
           <i class="fab fa-twitter fa-2x social"></i>
         </a>
       </div>
