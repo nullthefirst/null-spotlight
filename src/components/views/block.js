633c77e22abca0439a5c7beb82cac3f497ea1ebd
nullthefirst <jrandangana@gmail.com>
2018-12-01 14:07:35 +0100

    edit

--- src/components/views/block.js
+++ src/components/views/block.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
+import One from './one';
+
 export default class Block extends Component {
   render() {
     return(
       <div>
-        <a href="https://github.com/Usheninte/react-fcctest" alt="https://github.com/Usheninte/react-fcctest"><h4>react-fcctest Plugin <i class="em em-link"></i></h4></a>
-        <p>Open Source <b>react.js</b> plugin for the <b>freeCodeCamp.org</b> community. It enables developers to add the official freeCodeCamp Test Suite to their React application, and thus come closer to acquiring free software development certificates</p>
-
+        <One />
         <hr />
 
         <a href="https://usheninte.xyz/" alt="https://usheninte.xyz/"><h4>usheninte.xyz <i class="em em-link"></i></h4></a>
