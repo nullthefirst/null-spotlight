0e79b8c2e326c929e4705c641c4c645700340647
nullthefirst <jrandangana@gmail.com>
2018-12-01 11:46:20 +0100

    edited html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './components/App.css';
 
+import Block from './components/views/block';
+
 class App extends Component {
   render() {
     return (
       <div>
+        <Block />
       </div>
     );
   }
