8a46e2d36a2a51b94cd87bae258bbb669529e1d1
nullthefirst <jrandangana@gmail.com>
2018-12-01 13:05:55 +0100

    added emoji-css

--- src/components/views/block.js
+++ src/components/views/block.js
@@ -4,39 +4,39 @@ export default class Block extends Component {
   render() {
     return(
       <div>
-        <a href="https://github.com/Usheninte/react-fcctest" alt="https://github.com/Usheninte/react-fcctest"><h4>react-fcctest Plugin => <i class="em em-link"></i></h4></a>
+        <a href="https://github.com/Usheninte/react-fcctest" alt="https://github.com/Usheninte/react-fcctest"><h4>react-fcctest Plugin <i class="em em-link"></i></h4></a>
         <p>Open Source <b>react.js</b> plugin for the <b>freeCodeCamp.org</b> community. It enables developers to add the official freeCodeCamp Test Suite to their React application, and thus come closer to acquiring free software development certificates</p>
 
         <hr />
 
-        <a href="https://usheninte.xyz/" alt="https://usheninte.xyz/"><h4>usheninte.xyz => <i class="em em-link"></i></h4></a>
+        <a href="https://usheninte.xyz/" alt="https://usheninte.xyz/"><h4>usheninte.xyz <i class="em em-link"></i></h4></a>
         <p>A personal project. This is a personal portfolio <b>Single Page Application</b>. It contains my skillset, work experience proof in the form of real-world projects, and a fully functioning contact form.</p>
 
         <hr />
 
-        <a href="https://leadcityfashion.com.ng/" alt="https://leadcityfashion.com.ng/"><h4>Lead City Fashion & Lifestyle Academy => <i class="em em-link"></i></h4></a>
+        <a href="https://leadcityfashion.com.ng/" alt="https://leadcityfashion.com.ng/"><h4>Lead City Fashion & Lifestyle Academy <i class="em em-link"></i></h4></a>
         <p>This institution of Fashion and Lifestyle is upcoming, and promising to prove great. This academy boasts of seasoned professionals, world-class machinery and equipment, and a touch with inspiring and motivating future leaders like no other.</p>
 
         <hr />
           
-        <a href="https://dsc-app.lcuofficial.com.ng/" alt="https://dsc-app.lcuofficial.com.ng/"><h4>Developer Student Club LCU => <i class="em em-link"></i></h4></a>
+        <a href="https://dsc-app.lcuofficial.com.ng/" alt="https://dsc-app.lcuofficial.com.ng/"><h4>Developer Student Club LCU <i class="em em-link"></i></h4></a>
         <p>The DSC is a Google Developers program, aimed at empowering students in <b>Sub-Saharan Africa</b>, India and Indonesia with Mobile and Web Development skills. Other paradigms of <b>Software Development</b> are addressed. This is essentially a grass roots program.</p>
 
         <hr />
 
         <a href="https://lcu-jamcitylive-fest18.netlify.com/" alt="
-        https://lcu-jamcitylive-fest18.netlify.com/"><h4>JamCityLive LCU Festival of Music & Culture => <i class="em em-link"></i></h4></a>
+        https://lcu-jamcitylive-fest18.netlify.com/"><h4>JamCityLive LCU Festival of Music & Culture <i class="em em-link"></i></h4></a>
         <p>This was a weekend of amazement. Paintballing tournaments, beautiful food and a vibrant atmosphere. JamCityLive is an initiative that simply encourages the concept <b>“Learn by Doing”</b>. JamCityLive is all about educating young people. Educating in the sense that it is all about providing students with a platform to practice what they really want to become in the near future.</p>
 
         <hr />
 
         <a href="https://afrolumia.club/" alt="
-        https://afrolumia.club/"><h4>AfroLumia => <i class="em em-link"></i></h4></a>
+        https://afrolumia.club/"><h4>AfroLumia <i class="em em-link"></i></h4></a>
         <p>AfroLumia is a creative arts platform that actively supports innovative lifestyles. Think digital bohemians, think technologically innovative young people. Youth is a thing of the mind.</p>
           
         <hr />
 
-        <a href="https://poetrique.github.io/" alt="https://poetrique.github.io/"><h4>Poetrique Online => <i class="em em-link"></i></h4></a>
+        <a href="https://poetrique.github.io/" alt="https://poetrique.github.io/"><h4>Poetrique Online <i class="em em-link"></i></h4></a>
         <p>Poetrique is a creative arts brand, that focuses on creating unique user experiences and interactions for the web. We are the bridge between technology and art. We work with world-class technology to produce amazing results.</p>
 
         <hr />
