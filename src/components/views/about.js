c7e6ddee013c25a567059b9164c1ebc9a546ffdc
nullthefirst <jrandangana@gmail.com>
2018-12-01 21:53:32 +0100

    edited one.js

--- src/components/views/about.js
+++ src/components/views/about.js
@@ -6,11 +6,17 @@ const title = {
   color: "#31acce"
 }
 
+const round = {
+  width: "10em",
+  borderRadius: "50%"
+}
+
 export default class About extends Component {
   render() {
     return(
       <div>
         <h2 style={title}>About Me</h2>
+        <img style={round} src={'https://res.cloudinary.com/poetrique/image/upload/c_scale,h_500,w_500/v1543688520/miscellaneous/placeholder.png'} alt="Coder Image" title="Coder Profile" />
         <p className="message">Elementum sagittis vitae et leo. Non tellus orci ac auctor augue mauris augue neque gravida. Turpis egestas integer eget aliquet nibh. Risus quis varius quam quisque id diam. Lorem mollis aliquam ut porttitor leo a. Turpis egestas sed tempus urna et pharetra pharetra massa.</p>
         <p className="message">Orci eu lobortis elementum nibh tellus molestie nunc non. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Non nisi est sit amet. Facilisis magna etiam tempor orci eu lobortis. Orci sagittis eu volutpat odio .</p>
       </div>
--- src/components/views/git/one.js
+++ src/components/views/git/one.js
@@ -4,10 +4,10 @@ export default class One extends Component {
   render() {
     return(
       <div className="card">
-        <a href="https://github.com/Usheninte/react-fcctest" 
+        <a href="#" 
           target="_blank"
-          rel="noopener noreferrer" alt="https://github.com/Usheninte/react-fcctest"><h4>react-fcctest Plugin <i class="em em-link"></i></h4></a>
-        <p>Open Source <b>react.js</b> plugin for the <b>freeCodeCamp.org</b> community. It enables developers to add the official freeCodeCamp Test Suite to their React application, and thus come closer to acquiring free software development certificates</p>
+          rel="noopener noreferrer" alt="#"><h4>elementum integer <i class="em em-link"></i></h4></a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore <b>habitant morbi</b> magna aliqua. Vel elit scelerisque mauris pellentesque pulvinar pellentesque tristique. In dictum non consectetur a erat.</p>
       </div>
     )
   }
