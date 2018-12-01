6661d0c521256fffcaeba0f27370b1cdd2543798
nullthefirst <jrandangana@gmail.com>
2018-12-01 22:25:49 +0100

    revert to lorem ipsum

--- src/components/views/git/five.js
+++ src/components/views/git/five.js
@@ -6,8 +6,8 @@ export default class Five extends Component {
       <div className="card">
         <a href="https://poetrique.github.io" 
           target="_blank"
-          rel="noopener noreferrer" alt="#"><h4>JamCityLive LCU Festival of Music & Culture <i class="em em-link"></i></h4></a>
-        <p>This was a weekend of amazement. Paintballing tournaments, beautiful food and a vibrant atmosphere. JamCityLive is an initiative that simply encourages the concept <b>“Learn by Doing”</b>. JamCityLive is all about educating young people. Educating in the sense that it is all about providing students with a platform to practice what they really want to become in the near future.</p>
+          rel="noopener noreferrer" alt="#"><h4>nullam eget <i class="em em-link"></i></h4></a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio aenean sed adipiscing diam donec adipiscing.</p>
       </div>
     )
   }
