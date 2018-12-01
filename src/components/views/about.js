7b5f639f7b77358b241fba2ef5b3b218ffacca06
nullthefirst <jrandangana@gmail.com>
2018-12-01 20:39:16 +0100

    edited about.js

--- src/components/views/about.js
+++ src/components/views/about.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+const title = {
+  color: "#31acce"
+}
+
 export default class About extends Component {
   render() {
     return(
       <div>
-        <h1>About Me</h1>
+        <h2 style={title}>About Me</h2>
+        <p>Elementum sagittis vitae et leo. Non tellus orci ac auctor augue mauris augue neque gravida. Turpis egestas integer eget aliquet nibh. Risus quis varius quam quisque id diam. Lorem mollis aliquam ut porttitor leo a. Turpis egestas sed tempus urna et pharetra pharetra massa.</p>
+        <p>Orci eu lobortis elementum nibh tellus molestie nunc non. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Non nisi est sit amet. Facilisis magna etiam tempor orci eu lobortis. Orci sagittis eu volutpat odio .</p>
       </div>
     );
   }
